09db6aa34b8498309fa42fd64dcb9207590c5d93
himelv <63998835+melvnl@users.noreply.github.com>
2021-05-06 13:20:06 +0700

    :rocket: finishing most picked component

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react'
 import Header from 'components/Header'
 import Hero from 'components/Hero'
+import MostPicked from 'components/MostPicked'
+
 import HomepageData from 'json/homepage.json'
 
+
 export default class Homepage extends Component {
+    constructor(props){
+        super(props);
+        this.refMostPicked = React.createRef();
+    }
+    
+    
     render() { 
         return (
             <>
              <Header {...this.props}></Header>
-             <Hero data={HomepageData.hero} />   
+             <Hero refMostPicked={this.refMostPicked} data={HomepageData.hero} />  
+             <MostPicked refMostPicked={this.refMostPicked} data={HomepageData.mostPicked} />   
             </>
         )
     }
