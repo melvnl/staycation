8035f653cb327365096abb5ed258382e3de792d4
himelv <63998835+melvnl@users.noreply.github.com>
2021-05-13 22:09:45 +0700

    :rocket: finishing Input Date(Form) and Breadcrumb component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import "assets/scss/style.scss"
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Homepage from 'pages/Homepage'
-// import InputNumberExample from 'pages/InputNumberExample'
+import InputNumberExample from 'pages/Example'
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Route path="/" component={Homepage}></Route>
+        <Route path="/" component={InputNumberExample}></Route>
       </Router>
     </div>
   );
---
+++ src/pages/Example.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import InputDate from "elements/Form/InputDate";
+import Breadcrumb from "elements/Breadcrumb";
+export default class Example extends Component {
+  render() {
+    
+    const breadcrumb = [
+      {
+        pageTitle:"Home", pageHref: ""
+      },
+      {
+        pageTitle:"House Details", pageHref: ""
+      }
+    ]
+    return (
+      <div className="container">
+        <div
+          className="row align-items-center justify-content-center"
+          style={{ height: "100vh" }}
+        >
+          <div className="col-auto">
+            <Breadcrumb data={breadcrumb}/>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
